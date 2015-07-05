0ca33a8b1ca6cce783bbc9ed15971f4e75d29fc8
André Laszlo <andre@laszlo.nu>
2015-07-05 22:40:02 +0200

    Optimize python/sieve

--- python/sieve/sieve.py
+++ python/sieve/sieve.py
@@ -1,6 +1,9 @@
+from math import ceil, sqrt
+
 def sieve(n):
     """List all the primes between 2 and n (inclusively)."""
     p = list(range(2, n+1))
-    for i in range(2, n+1):
+    limit = int(ceil(sqrt(n))) + 1
+    for i in range(2, limit):
         p = [x for x in p if x == i or x % i != 0]
     return p
