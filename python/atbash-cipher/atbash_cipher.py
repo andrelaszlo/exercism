70b809018cb5ce47e037928229d4628f28ab03b7
André Laszlo <andre@laszlo.nu>
2015-07-09 20:45:20 +0200

    Fix tests for python/atbash-cipher

--- python/atbash-cipher/atbash_cipher.py
+++ python/atbash-cipher/atbash_cipher.py
@@ -1,18 +1,26 @@
 from codecs import register, CodecInfo
 from string import ascii_lowercase, maketrans, translate
+import re
 
 # Register an atbash encoding
 # Would be in a separate module, but exercism doesn't support submitting
 # multiple files (yet!)
 
+_pattern = re.compile(r'[^a-z1-9]')
+
 _trans = maketrans(ascii_lowercase, ascii_lowercase[::-1])
 
-def _atbash(input, _=None):
-    return translate(input.lower(), _trans), len(input)
+def _atbash(input, group=False):
+    raw = translate(_pattern.sub('', input.lower()), _trans)
+    if not group:
+        return raw, len(raw)
+    grouped = [raw[i:i+5] for i in range(0, len(raw), 5)]
+    return ' '.join(grouped), len(input)
 
 def _search(name):
     if name == 'atbash':
-        return CodecInfo(_atbash, _atbash)
+        return CodecInfo(lambda s, _=None: _atbash(s, True),
+                         lambda s, _=None: _atbash(s, False))
     return None
 
 register(_search)
@@ -22,5 +30,6 @@ register(_search)
 def encode(clear):
     return clear.encode('atbash')
 
-decode = encode  # This cipher is symmetrical
+def decode(cypher):
+    return cypher.decode('atbash')
 
--- python/atbash-cipher/atbash_cipher_test.py
+++ python/atbash-cipher/atbash_cipher_test.py
@@ -15,30 +15,30 @@ class AtbashCipherTest(unittest.TestCase):
         self.assertEqual("lnt", encode("OMG"))
 
     def test_encode_O_M_G(self):
-        self.assertEqual("l n t", encode("O M G"))
+        self.assertEqual("lnt", encode("O M G"))
 
     def test_encode_long_word(self):
-        self.assertEqual("nrmwyoldrmtob", encode("mindblowingly"))
+        self.assertEqual("nrmwy oldrm tob", encode("mindblowingly"))
 
     def test_encode_numbers(self):
-        self.assertEqual("gvhgrmt, 1 2 3, gvhgrmt.",
+        self.assertEqual("gvhgr mt123 gvhgr mt",
                          encode("Testing, 1 2 3, testing."))
 
     def test_encode_sentence(self):
-        self.assertEqual("gifgs rh urxgrlm.",
+        self.assertEqual("gifgs rhurx grlm",
                          encode("Truth is fiction."))
 
     def test_encode_all_things(self):
         plaintext = "The quick brown fox jumps over the lazy dog."
-        ciphertext = "gsv jfrxp yildm ulc qfnkh levi gsv ozab wlt."
+        ciphertext = "gsvjf rxpyi ldmul cqfnk hlevi gsvoz abwlt"
         self.assertEqual(ciphertext, encode(plaintext))
 
     def test_decode_word(self):
-        self.assertEqual("exercism", decode("vcvixrhn"))
+        self.assertEqual("exercism", decode("vcvix rhn"))
 
     def test_decode_sentence(self):
         self.assertEqual("anobstacleisoftenasteppingstone",
-                         decode("zmlyhgzxovrhlugvmzhgvkkrmthglmv"))
+                         decode("zmlyh gzxov rhlug vmzhg vkkrm thglm v"))
 
 
 if __name__ == '__main__':
