8390efac2a5d6574a87bea606ff5c71b6c846da2
André Laszlo <andre@laszlo.nu>
2015-07-09 19:50:40 +0200

    Refactor python/atbash-cipher into an encoding

--- python/atbash-cipher/atbash_cipher.py
+++ python/atbash-cipher/atbash_cipher.py
@@ -1,8 +1,7 @@
-from string import ascii_lowercase, maketrans, translate
-
-table = maketrans(ascii_lowercase, ascii_lowercase[::-1])
+import atbash_encoding
 
 def encode(clear):
-    return translate(clear.lower(), table)
+    return clear.encode('atbash')
 
 decode = encode  # This cipher is symmetrical
+
