eb5bc9f3e38268a457fa861f8551adfa4e48bdbe
André Laszlo <andre@laszlo.nu>
2015-07-09 19:53:43 +0200

    Refactor python/atbash-cipher into one file again

--- python/atbash-cipher/atbash_cipher.py
+++ python/atbash-cipher/atbash_cipher.py
@@ -1,4 +1,23 @@
-import atbash_encoding
+from codecs import register, CodecInfo
+from string import ascii_lowercase, maketrans, translate
+
+# Register an atbash encoding
+# Would be in a separate module, but exercism doesn't support submitting
+# multiple files (yet!)
+
+_trans = maketrans(ascii_lowercase, ascii_lowercase[::-1])
+
+def _atbash(input, _=None):
+    return translate(input.lower(), _trans), len(input)
+
+def _search(name):
+    if name == 'atbash':
+        return CodecInfo(_atbash, _atbash)
+    return None
+
+register(_search)
+
+# Module interface
 
 def encode(clear):
     return clear.encode('atbash')
