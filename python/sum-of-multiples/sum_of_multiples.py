cff38f1a5d8f6fd69496506418f9293d88ad4537
André Laszlo <andre@laszlo.nu>
2015-07-11 16:11:33 +0200

    Refactor python sum-of-multiples to a oneliner

--- python/sum-of-multiples/sum_of_multiples.py
+++ python/sum-of-multiples/sum_of_multiples.py
@@ -1,6 +1,4 @@
-def _multiples(n, m):
-    if m == 0: return set()
-    return set(i*m for i in range(1, int(n/m)+1) if i*m < n)
-
 def sum_of_multiples(n, factors=[3, 5]):
-    return sum(reduce(set.union, [_multiples(n, m) for m in factors]))
+    return sum(reduce(set.union,
+                      [set(range(m, n, m)) if m != 0 else set()
+                       for m in factors]))
